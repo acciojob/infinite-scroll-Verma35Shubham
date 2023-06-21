f204a34a174fd57f9465f32e6df6f0565e619183
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-21 16:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here!
 
+let ol = document.getElementById("infi-list")
+let item = 20
+
+for(let i=1;i<=item;i++){
+	let li = document.createElement("li")
+	li.innerText = "item "+i
+	ol.append(li)
+}
